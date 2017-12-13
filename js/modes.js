6e29bbd354a82852039fc6420df956811d1b255c
rickytranmer <rwtranmer@gmail.com>
2017-12-13 14:33:23 -0700

    adds new game mode to intro screen

--- js/modes.js
+++ js/modes.js
@@ -1,13 +1,30 @@
 console.log('JS works');
 
 var twoPlayerMode, sharedMode = true;
-var gameMode = 'game.html'
+var gameMode = 'game.html';
 
-function startTheGame () {
+function startTheGame() {
 	window.location = gameMode;
 }
 
+function separateGame() {
+	//TODO - Check if 2player/CPU
+	gameMode = 'gameSplit.html'
+	document.getElementById('separate-score').classList.add('active');
+	document.getElementById('shared-score').classList.remove('active');
+}
+
+function sharedGame() {
+	//TODO - Check if 2player/CPU
+	gameMode = 'game.html'
+	document.getElementById('shared-score').classList.add('active');
+	document.getElementById('separate-score').classList.remove('active');
+}
+
 document.getElementById('start-button').addEventListener("click", startTheGame);
+
+document.getElementById('separate-score').addEventListener("click", separateGame);
+document.getElementById('shared-score').addEventListener("click", sharedGame);
 
 //TODO - Add event listeners for each button to change gameMode
 //TODO										 to add/remove 'active' class
